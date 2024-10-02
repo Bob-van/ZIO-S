dafb55e1bf81926668693ef9d9aa748f61da3975
Robert Vandrovec <robert@vandrovec.eu>
2024-10-02 23:19:31 +0200

    General default setup

---
+++ build.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const rlz = @import("raylib-zig");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = .ReleaseFast;
+    const src_dir = "src/";
+
+    const exe = b.addExecutable(.{
+        .name = "ZIO-S",
+        .root_source_file = b.path(src_dir ++ "main.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run ZIO-S");
+    run_step.dependOn(&run_cmd.step);
+
+    b.installArtifact(exe);
+}
